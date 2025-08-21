068602ef5a156690be31d169dfb26440231568a8
Angel Eduardo Diaz Mendiburu <warydiaz@hotmail.com>
2025-08-21 10:42:22 +0200

    Update API endpoints in login and signup pages to include port numbers

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,7 +13,7 @@ export default function LoginPage() {
     setError("");
 
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_URL_BACKEND}/login`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_URL_BACKEND}:${process.env.NEXT_PUBLIC_PORT_LOGIN}/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -39,7 +39,7 @@ export default function RegisterPage() {
     setErrors({});
 
     try {
-      const res = await fetch(`${process.env.URL_BACKEND}/user`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_URL_BACKEND}:${process.env.NEXT_PUBLIC_PORT_TOURNAMENT}/user`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password, name, surname, role }),
